9f1950859533cb6f3e59d3d3141fabbf8ca6343d
avraam311 <yandaroviba@mail.ru>
2024-10-18 21:34:56 +0300

    psql settings fix begore deploy

--- config.py
+++ config.py
@@ -11,6 +11,11 @@ class Config:
         self.telegram_admin_id: int = int(os.getenv('TELEGRAM_ADMIN_ID'))
         self.payments_token: str = os.getenv('PAYMENTS_TOKEN')
         self.psql_url: str = os.getenv('PSQL_URL')
+        self.psql_user: str = os.getenv('PSQL_USER')
+        self.psql_password: str = os.getenv('PSQL_PASSWORD')
+        self.psql_host: str = os.getenv('PSQL_HOST')
+        self.psql_port: int = int(os.getenv('PSQL_PORT'))
+        self.psql_db: str = os.getenv('PSQL_DB')
         self.redis_username: str = os.getenv('REDIS_USERNAME')
         self.redis_password: str = os.getenv('REDIS_PASSWORD')
         self.redis_host: str = os.getenv('REDIS_HOST')
@@ -25,6 +30,16 @@ class Config:
             raise ValueError("PAYMENTS_TOKEN не установлен")
         if not self.psql_url:
             raise ValueError("PSQL_URL не установлен")
+        if not self.psql_user:
+            raise ValueError("PSQL_USER не установлен")
+        if not self.psql_password:
+            raise ValueError("PSQL_PASSWORD не установлен")
+        if not self.psql_host:
+            raise ValueError("PSQL_HOST не установлен")
+        if not self.psql_port:
+            raise ValueError("PSQL_PORT не установлен")
+        if not self.psql_db:
+            raise ValueError("PSQL_DB не установлен")
         if not self.redis_username:
             raise ValueError("REDIS_USERNAME не установлен")
         if not self.redis_password:
--- database/engine.py
+++ database/engine.py
@@ -9,7 +9,7 @@ import config
 
 config = config.Config()
 
-engine = create_async_engine(url=config.psql_url, echo=True)
+engine = create_async_engine(url=config.psql_url, echo=True,)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
